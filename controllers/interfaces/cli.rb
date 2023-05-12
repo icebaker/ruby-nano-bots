8ffb3393cfcbb88b7f09df4f74d1cb756ab311da
icebaker <icebaker@proton.me>
2023-05-11 21:10:13 -0300

    adding help and version commands

--- controllers/interfaces/cli.rb
+++ controllers/interfaces/cli.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 require_relative '../instance'
+require_relative '../../static/gem'
 
 module NanoBot
   module Controllers
     module Interfaces
       module CLI
         def self.handle!
+          case ARGV[0]
+          when 'version'
+            puts NanoBot::GEM[:version]
+            exit
+          when 'help', '', nil
+            puts "Ruby Nano Bots #{NanoBot::GEM[:version]}"
+            puts '  rnb cartridge.yml - eval "Hello!"'
+            puts '  rnb cartridge.yml - repl'
+            puts '  rnb cartridge.yml - debug'
+            puts '  rnb cartridge.yml STATE-KEY eval "Hello!"'
+            puts '  rnb cartridge.yml STATE-KEY repl'
+            puts '  rnb cartridge.yml STATE-KEY debug'
+            puts '  rnb version'
+            exit
+          end
+
           params = { cartridge_path: ARGV[0], state: ARGV[1], command: ARGV[2] }
 
           bot = Instance.new(cartridge_path: params[:cartridge_path], state: params[:state])
