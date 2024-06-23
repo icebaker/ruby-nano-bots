99ceadafd7c98c1d200cea2f138723ae96716868
icebaker <icebaker@proton.me>
2024-06-23 18:32:19 -0300

    adding claude cartridges

--- static/gem.rb
+++ static/gem.rb
@@ -4,7 +4,7 @@ module NanoBot
   GEM = {
     name: 'nano-bots',
     version: '3.3.0',
-    specification: '3.1.0',
+    specification: '3.2.0',
     author: 'icebaker',
     summary: 'Ruby Implementation of Nano Bots: small, AI-powered bots for OpenAI ChatGPT, Ollama, Mistral AI, Anthropic Claude, Cohere Command, Maritaca AI MariTalk, and Google Gemini.',
     description: 'Ruby Implementation of Nano Bots: small, AI-powered bots that can be easily shared as a single file, designed to support multiple providers such as OpenAI ChatGPT, Ollama, Mistral AI, Cohere Command, Maritaca AI MariTalk, and Google Gemini, with support for calling Tools (Functions).',
